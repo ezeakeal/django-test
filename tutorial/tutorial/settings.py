bd83eb6e40f4678420e4ad4ecce4d0ee42fa8f90
Daniel Vagg <ezeakeal@gmail.com>
2018-11-06 23:58:44 +0000

    Adding celery config

--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -118,3 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+#################
+# Celery Config
+#################
+CELERY_BROKER_URL = 'redis://localhost'
+
